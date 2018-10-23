0eeec7ec87462e411f4d5fd75b0ed30a5c4af429
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-23 11:20:59 -0600

    ledger

--- src/components/Ledger.js
+++ src/components/Ledger.js
@@ -1,9 +1,14 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Transactions from './Transactions'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  flex: 2
+`
 
 const Ledger = () => (
-  <div>
-    <Transactions /><
-  </div>
+  <Container>
+    <Transactions />
+  </Container>
 )
 export default Ledger
