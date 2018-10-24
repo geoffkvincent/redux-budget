8e06b82c1044f17dbb8253f25d12286df5a6d1e1
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-24 10:35:25 -0600

    import middleware

--- src/index.js
+++ src/index.js
@@ -4,20 +4,24 @@ import App from './components/App'
 import { Provider } from 'react-redux'
 import store from './store'
 import { ThemeProvider } from 'styled-components'
-import { initMiddleware } from 'devise-axios'<
+import { initMiddleware } from 'devise-axios'
+import { BrowserRouter } from 'react-router-dom'
 
 initMiddleware()
 
 const theme = {
   blue: '#B0E2FF',
   green: '#D6FFD6',
+  red: '#FFCCCC',
 }
 
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('app')
 )
